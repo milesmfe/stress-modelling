032df404ae6c7997229bd0da57b8152e2ca420de
Miles Edwards <miles.mfe@gmail.com>
2025-04-24 09:06:53 +0100

    Add models QUANT and TSF

--- models/model_registry.py
+++ models/model_registry.py
@@ -6,8 +6,9 @@ Add new models here and use by name.
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.neighbors import KNeighborsClassifier
-from sktime.classification.interval_based import CanonicalIntervalForest
+from sktime.classification.interval_based import CanonicalIntervalForest, TimeSeriesForestClassifier
 from xgboost import XGBClassifier
+from aeon.classification.interval_based import QUANTClassifier
 
 MODEL_REGISTRY = {
     "RandomForest": lambda: RandomForestClassifier(n_estimators=100, random_state=42),
@@ -15,6 +16,9 @@ MODEL_REGISTRY = {
     "KNN": lambda: KNeighborsClassifier(n_neighbors=5),
     "XGBoost": lambda: XGBClassifier(use_label_encoder=False, eval_metric='mlogloss'),
     "CanonicalIntervalForest": lambda: CanonicalIntervalForest(random_state=42),
+    "TimeSeriesForest": lambda: TimeSeriesForestClassifier(random_state=42),
+    "QUANT": lambda: QUANTClassifier(random_state=42),
+    # Add more models here
 }
 
 def get_model(name: str):
